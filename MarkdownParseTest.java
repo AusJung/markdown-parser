7daadf5006f4cec7879845dedaf9a02156544593
AusJung <aujung@ucsd.edu>
2022-04-29 18:44:04 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -12,19 +12,20 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
     @Test
-    // public void getLinks() throws IOException {
-    //     try {
-    //         Path fileName = Path.of("test-file.md");
-    //         String content = Files.readString(fileName);
-    //         ArrayList<String> links = MarkdownParse.getLinks(content);
-    //         ArrayList<String> actual = new ArrayList<>();
-    //         actual.add("https://something.com, some-thing.html");
-    //         assertEquals(actual.toString(), links.toString());
+    public void getLinks() throws IOException {
+        try {
+            Path fileName = Path.of("test-file.md");
+            String content = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(content);
+            ArrayList<String> actual = new ArrayList<>();
+            actual.add("https://something.com, some-thing.html");
+            assertEquals(actual.toString(), links.toString());
             
-    //     }
-    //     catch (Exception e) {
-    //         //TODO: handle exception
-    //         System.out.println("catch");
-    //     }
-    // }
+        }
+        catch (Exception e) {
+            //TODO: handle exception
+            System.out.println("catch");
+            assertFalse(false);
+        }
+    }
 }
